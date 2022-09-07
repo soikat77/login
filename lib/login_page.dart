ad86bfc52c7ebcb974b37294c1fb405f824832d6
soikat77 <soikatalam26@gmail.com>
2022-09-07 17:13:24 +0600

    Log in page ok

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -14,8 +14,199 @@ class _LoginPageState extends State<LoginPage> {
       appBar: AppBar(
         title: const Text('Log In'),
       ),
-      body: const Center(
-        child: Text('Log in to enter the system'),
+      backgroundColor: Colors.grey[300],
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              children: [
+                // TODO: Greetings
+                const SizedBox(height: 48),
+                const Icon(
+                  Icons.android_outlined,
+                  size: 75,
+                  color: Colors.blue,
+                ),
+                const SizedBox(height: 48),
+                Text(
+                  'Hello Again!',
+                  style: TextStyle(
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blue[900]),
+                ),
+                const SizedBox(height: 12),
+                const Text(
+                  'Wellcome Back. We have been missed.',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.normal,
+                  ),
+                ),
+                const SizedBox(height: 32),
+
+                // TODO: Email
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.grey[200],
+                      border: Border.all(color: Colors.white),
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    child: const Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 0),
+                      child: TextField(
+                        enabled: true, // to trigger disabledBorder
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Color(0xFFF2F2F2),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(5)),
+                            borderSide: BorderSide(
+                                width: 2, color: Colors.lightBlueAccent),
+                          ),
+                          disabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(5)),
+                            borderSide:
+                                BorderSide(width: 1, color: Colors.orange),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(5)),
+                            borderSide:
+                                BorderSide(width: 1, color: Colors.white),
+                          ),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(5)),
+                            borderSide: BorderSide(width: 1),
+                          ),
+                          errorBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(5)),
+                            borderSide: BorderSide(width: 1, color: Colors.red),
+                          ),
+                          focusedErrorBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(5)),
+                            borderSide: BorderSide(
+                                width: 1, color: Colors.yellowAccent),
+                          ),
+                          hintText: "Email",
+                          hintStyle:
+                              TextStyle(fontSize: 16, color: Color(0xFFB3B1B1)),
+                          // errorText: snapshot.error,
+                        ),
+                        // controller: _passwordController,
+                        // onChanged: _authenticationFormBloc.onPasswordChanged,
+                        // obscureText: false,
+                      ),
+                    ),
+                  ),
+                ),
+
+                const SizedBox(height: 12),
+
+                //TODO: Password
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.grey[200],
+                      border: Border.all(color: Colors.white),
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    child: const Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 0),
+                      child: TextField(
+                        enabled: true, // to trigger disabledBorder
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Color(0xFFF2F2F2),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(5)),
+                            borderSide: BorderSide(
+                                width: 2, color: Colors.lightBlueAccent),
+                          ),
+                          disabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(5)),
+                            borderSide:
+                                BorderSide(width: 1, color: Colors.orange),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(5)),
+                            borderSide:
+                                BorderSide(width: 1, color: Colors.white),
+                          ),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(5)),
+                            borderSide: BorderSide(width: 1),
+                          ),
+                          errorBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(5)),
+                            borderSide: BorderSide(width: 1, color: Colors.red),
+                          ),
+                          focusedErrorBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(5)),
+                            borderSide: BorderSide(
+                                width: 1, color: Colors.yellowAccent),
+                          ),
+                          hintText: "Password",
+                          hintStyle:
+                              TextStyle(fontSize: 16, color: Color(0xFFB3B1B1)),
+                          // errorText: snapshot.error,
+                        ),
+                        // controller: _passwordController,
+                        // onChanged: _authenticationFormBloc.onPasswordChanged,
+                        obscureText: true,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 18),
+
+                // TODO: Sign in Button
+
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.blue[900],
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    child: const Center(
+                      child: Padding(
+                        padding: EdgeInsets.all(12.0),
+                        child: Text(
+                          'Sign In',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+
+                const SizedBox(height: 32),
+
+                // TODO: Register
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text('Not a member? '),
+                    Text(
+                      'Register Now',
+                      style: TextStyle(
+                        color: Colors.blue[900],
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
